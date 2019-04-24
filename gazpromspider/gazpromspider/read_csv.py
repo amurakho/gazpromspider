52f49a2457ce9c3a9a30b2e9b3e3fbe542d14cd9
Artem Murakhovskyi <amurakho@gmail.com>
2019-04-24 12:51:27 +0300

    after test - eding read_scv.py(path)

--- gazpromspider/gazpromspider/read_csv.py
+++ gazpromspider/gazpromspider/read_csv.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 if __name__ == '__main__':
-    df = pd.read_csv('gazpromspider/base.csv')
+    df = pd.read_csv('base.csv')
 
     writer = pd.ExcelWriter('data.xlsx')
     df.to_excel(writer)
